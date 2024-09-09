e9261444fa38b3f6b3079ce559f3448cb4af21a8
bartugs <117641579+bartugs@users.noreply.github.com>
2024-09-09 16:34:41 +0200

    fizz

--- src/main/java/nl/han/ica/oose/dea/testedfizzbuzz/FizzBuzzExecutor.java
+++ src/main/java/nl/han/ica/oose/dea/testedfizzbuzz/FizzBuzzExecutor.java
@@ -3,6 +3,16 @@ package nl.han.ica.oose.dea.testedfizzbuzz;
 public class FizzBuzzExecutor {
 
     public String execute(int i){
-        return null;
+        String answr = "";
+        if (i % 3 == 0) {
+            answr += "Fizz";
+        }
+        if (i % 5 == 0) {
+            answr += "Buzz";
+        }
+        if(answr.isEmpty()){
+            answr = String.valueOf(i);
+        }
+        return answr;
     }
 }
